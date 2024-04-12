e7fa66f15bb122b551d17b7fb3d3cca9904ac140
Jaeeun <pjcson8@naver.com>
2024-04-12 15:40:59 +0900

    first commit

---
+++ src/main/java/com/example/swip/SwipApplication.java
@@ -0,0 +1,13 @@
+package com.example.swip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/swip/SwipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
